f75fb6b88292013becee8e9b1158001fad609c62
Gustav Utke Kauman <gustav@kauman.dk>
2025-09-28 23:40:51 +0200

    neovim v0.11 lsp support

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,45 +1,55 @@
-local lsp = require("lsp-zero")
+local cmp = require('cmp')
+local mason = require("mason-lspconfig")
 
-lsp.preset("recommended")
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'}
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+    })
+})
 
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'rust_analyzer',
+require("mason").setup({})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local servers = {
+    'ts_ls',
+    'angularls',
+    'eslint',
+    'rust_analyzer',
     'svelte',
     'omnisharp'
+}
+
+mason.setup({
+    ensure_installed = servers
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
-})
+for _, server in ipairs(servers) do
+    vim.lsp.config(server, {
+        capabilities = capabilities,
 
-lsp.set_preferences({
-	sign_icons = { }
-})
+        on_attach = function(client, bufnr)
+            local opts = {buffer = bufnr, remap = false}
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        end
+    })
+end
 
-lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
-
-lsp.setup()
--- nvim/.config/nvim/lua/gustavkauman/packer.lua
+++ nvim/.config/nvim/lua/gustavkauman/packer.lua
@@ -21,32 +21,18 @@ return require('packer').startup(function(use)
         {run = ':TSUpdate'}
     )
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
+    use {
+		'williamboman/mason-lspconfig.nvim',
+        requires = {
 			{'neovim/nvim-lspconfig'},
 			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},
 			{'hrsh7th/cmp-buffer'},
 			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'hrsh7th/cmp-nvim-lua'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		},
-        branch = 'v2.x'
-	}
-
-    use {
-        '/Users/gkauman/dev/al-neovim/',
-        opt = true
+        }
     }
 
     use 'rust-lang/rust.vim'
